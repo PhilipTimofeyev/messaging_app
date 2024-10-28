0ee9d4a3be2be00ec7aa39b633d8f419b0a54a4f
Philip Timofeyev <philiptimofeyev@gmail.com>
2024-10-27 20:08:02 -0700

    Refactor sign out to use axios

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,25 @@
 import { React, useState, useEffect } from 'react'
-import { Link, useLocation } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styles from './NavBar.module.css'
-import { useApi } from '../hooks/useApi.js'
-import { Navigate } from 'react-router-dom';
+import axios from "axios";
 
 function NavBar({ user }) {
-  const [currentPath, setCurrentPath] = useState(useLocation().pathname)
-    const url = 'http://127.0.0.1:3000/users/tokens/revoke'
-    const [requestOptions, setRequestOptions] = useState()
-    
-    const { data, isLoading, error } = useApi(url, requestOptions)
+  const navigate = useNavigate()
+  const accessToken = localStorage.getItem('accessToken')
 
-    async function handleSignOut() {
-      const accessToken = localStorage.getItem('accessToken');
-        const requestOptions = {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json', "Authorization": "Bearer " + accessToken },
-        };
-        setRequestOptions(requestOptions)
-        localStorage.removeItem("token")
+  const axiosInstance = axios.create({
+    baseURL: "http://127.0.0.1:3000/",
+    headers: {
+      "Content-type": "application/json",
+      'Authorization': 'Bearer ' + accessToken
     }
+  });
+
+  function handleSignOut() {
+    axiosInstance.post('/users/tokens/revoke')
+    localStorage.removeItem("token")
+    navigate('/signin')
+  }
 
   return (
     <div className={styles.navBar}>
@@ -28,7 +28,6 @@ function NavBar({ user }) {
           <Link to='/'>Main</Link>
           <Link to='/profile'>Profile</Link>
           <Link onClick={handleSignOut}>Sign Out</Link>
-          {requestOptions && (<Navigate to="/signin" replace={true} />)}
         </li>
       </ul>
     </div>
