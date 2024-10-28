515489db35fd2eebe6864279c05da05bb6e43130
Philip Timofeyev <philiptimofeyev@gmail.com>
2024-10-28 15:17:33 -0700

    Create active search for users via input

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,16 +1,47 @@
-import React from 'react'
+import { React, useState } from 'react'
 
-function Users({users}) {
+function Users({ users }) {
 
-  const listUsers = users.map(user => 
+  const [selectedUsers, setSelectedUsers] = useState([])
+  const [userList, setUserList] = useState([])
+  const [inputValue, setInputValue] = useState(null)
+
+  function handleClick(userId) {
+    const selectedUser = users.find((user) => user.id === userId)
+    setSelectedUsers([...selectedUsers, selectedUser])
+  }
+
+  const listSelectedUsers = selectedUsers.map(user =>
     <li key={user.id}>
       <p>{user.email}</p>
     </li>
   )
 
+  function findUsers(value) {
+
+    if (value === '') return setUserList([])
+
+    const result = users.filter((user) => user.email.includes(value))
+    setUserList(result)
+  }
+
+  const listUsers = userList.map(user => 
+    <li key={user.id}>
+      <p onClick={() => handleClick(user.id)}>{user.email}</p>
+    </li>
+  )
+
   return (
     <div>
-      <ul>{listUsers}</ul>
+      <label>
+        Text input: 
+        <input 
+        name="myInput"
+          onChange={e => findUsers(e.target.value)}
+        />
+      </label>
+      <ul>{userList && listUsers}</ul>
+      <ul>{listSelectedUsers}</ul>
     </div>
   )
 }
