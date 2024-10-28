54f5c20670ebf8249b9c50fb7628a1881855e635
Philip Timofeyev <philiptimofeyev@gmail.com>
2024-10-27 19:57:06 -0700

    Clean up

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,36 +1,9 @@
 import { React, useState, useEffect } from 'react'
 import NavBar from './NavBar';
 import { Link, Outlet, useNavigate, useLocation } from "react-router-dom";
-import { useApi } from '../hooks/useApi.js'
 
 
 function MainPage({ user }) {
-  const navigate = useNavigate();
-  const [requestOptions, setRequestOptions] = useState()
-  
-  const url = "http://127.0.0.1:3000/messages"
-  const { data, isLoading, error } = useApi(url, requestOptions)
-  
-  const storedToken = localStorage.getItem('token')
-
-  function handleClick() {
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json', "Authorization": "Bearer " + storedToken },
-      body: JSON.stringify(
-        {
-          "message":
-          {
-            "content": "Message creation works!"
-          }
-        }
-      )
-    };
-    setRequestOptions(requestOptions)
-    
-  }
-
-  if (error) navigate("/signin", { state: { error }})
 
   return (
     <>
@@ -38,7 +11,6 @@ function MainPage({ user }) {
         <NavBar user={user}/>
       </nav>
       <Outlet/>
-      <button onClick={handleClick}>Test</button>
     </>
   )
 }
