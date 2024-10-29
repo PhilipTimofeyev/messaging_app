77f49b90b68215c1f3dc1159af8a033e03a46d0f
Philip Timofeyev <philiptimofeyev@gmail.com>
2024-10-29 12:19:12 -0700

    Update styling

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -20,11 +20,20 @@ function Messages({ user, currentGroup, refreshGroup }) {
         return newMessage
     }
 
+    const listUsers = currentGroup.users.map((user) => {
+        return (
+            <li key={user.id}>
+                <h4>{user.email}</h4>
+            </li>
+        )
+    })
+
   return (
     <div className={styles.messagesContainer}>
+        <ul className={styles.messageUsers}>{listUsers}</ul>
         {currentGroup && <MessagesWindow user={user} currentGroup={currentGroup}/>}
         <form onSubmit={handleSubmit} className={styles.form}>
-              <input type='content' name='content'></input>
+            <input type='content' name='content'></input>
             <button type="submit">Send</button>
         </form>
     </div>
@@ -39,17 +48,20 @@ function MessagesWindow({currentGroup, user}) {
         const isCurrentUser = user.id == message.user_id
         return (
             <li key={message.id} className={isCurrentUser ? styles.currentUserMessage : styles.otherUserMessage}>
-                <p>{messageUser.email}</p>
-                <p>{message.content}</p>
+                <h5>{messageUser.email}</h5>
+                <p className={styles.messageContent}>{message.content}</p>
             </li>
         )
     })
 
     return (
+        <>
         <div className={styles.messagesWindow}>
             <ul>{listMessages}</ul>
         </div>
+        </>
     )
 }
+
 
 export default Messages
